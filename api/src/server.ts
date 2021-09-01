3276282baa419fbac39b6a81a73c422d5dee907b
Darren S <dcsuroviec@gmail.com>
2021-09-01 14:34:03 -0400

    Removed local data file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,6 @@
 import express from "express";
-import { readFileSync } from "fs";
-import path from "path";
-// import getUsers from "./src/databasepg";
 import { getUsers } from "./databasepg";
 const app = express();
-
-// LOCAL DATA IN DATA.JSON TO TEST RESPONSE TO LOCALHOST:3000 FETCH
-// app.get("/api/users", (req, res) => {
-//     const products = JSON.parse(
-//         readFileSync(path.join(__dirname, "./data.json"), "utf8")
-//     );
-//     console.log(JSON.stringify(products));
-//     res.send(products);
-// });
 
 // GET USERS FROM FETCH-USERS-DB POSTGRES
 app.get("/api/users", (req, res, next) => {

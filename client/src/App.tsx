e48c3cab0db03125e68d29126d4cdfb568b59035
Darren S <dcsuroviec@gmail.com>
2021-09-10 11:24:07 -0400

    Add context, token, localstorage, sql prepared statments, update routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
     Route,
     Link,
@@ -10,45 +10,58 @@ import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { ContactUs } from "./components/ContactUs";
 import "./App.css";
+import TokenContext from "./contexts/TokenContext";
 
 function App() {
-    // If there is no token cookie, redirect to Login
-    const [token, setToken] = useState<null | string>(null);
-
-    console.log(token, "token");
+    const [token, setToken] = useState<null | string>(
+        localStorage.username || null
+    );
 
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/contactUs">Contact Us</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <Switch>
-                <Route exact path="/">
-                    <Home token={token} />
-                </Route>
-                <Route path="/contactUs">
-                    <ContactUs token={token} />
-                </Route>
-                <Route path="/login">
-                    <Login token={token} setToken={setToken} />
-                </Route>
-                <Route>
-                    <Redirect to="/" />
-                </Route>
-            </Switch>
-        </Router>
+        <TokenContext.Provider value={{ token, setToken }}>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/contactUs">Contact Us</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="#0"
+                                    onClick={() => {
+                                        setToken(null);
+                                        localStorage.removeItem("username");
+                                    }}
+                                >
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/contactUs">
+                        <ContactUs />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route>
+                        <Redirect to="/" />
+                    </Route>
+                </Switch>
+            </Router>
+        </TokenContext.Provider>
     );
 }
 
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import { useContext, useState } from "react";
 import { Redirect } from "react-router-dom";
+import TokenContext from "../contexts/TokenContext";
+export const Login = () => {
+    const [username, setUsername] = useState<string | null>();
+    const [password, setPassword] = useState<string | number>();
 
-type LoginProps = {
-    token: string | null;
-    setToken: Function;
-};
-
-export const Login = ({ token, setToken }: LoginProps) => {
-    const handleLogin = () => {
-        // If authentication passes, set token, which redirects the component
-        setToken("some token");
-    };
+    const { token, setToken } = useContext(TokenContext)!;
+    interface User {
+        id: number;
+        firstname: string;
+        lastname: string;
+        username: string;
+    }
 
     return (
         <>
@@ -26,13 +27,37 @@ export const Login = ({ token, setToken }: LoginProps) => {
                 <form onSubmit={(event) => event.preventDefault()}>
                     <label>
                         <p>Username</p>
-                        <input type="text" />
+                        <input
+                            type="text"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </label>
                     <label>
                         <p>Password</p>
-                        <input type="password" />
+                        <input
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </label>
-                    <button type="submit" onClick={() => handleLogin()}>
+                    <button
+                        type="submit"
+                        onClick={async () => {
+                            const token: string = await fetch("/api/token", {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({ username }),
+                            }).then((response) => response.json());
+
+                            // If user exists, set context
+                            console.log(token);
+                            if (username && token.length) {
+                                setToken(token);
+                                localStorage.setItem("username", username);
+                            }
+                        }}
+                    >
                         Set Token To Truthy
                     </button>
                 </form>
